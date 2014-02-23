1a4e47253756a3add344b9e6d76bc21b06d3f1d8
Andy Niles <aniles@gmail.com>
2014-02-23 10:59:45 -0500

    adding chapter 2 exercises

---
+++ chapter_2.rb
@@ -0,0 +1,11 @@
+#Display how many hours are in a year
+puts 'There are ' + (24*365).to_s + ' hours in a year.'
+
+#Display how many minutes are in a decade
+puts 'There are ' + (60*24*365*10).to_s + ' minutes in a decade.'
+
+#Display your age (29) in seconds
+puts '29 years old = ' + (29*365*24*60*60).to_s + ' seconds old.'
+
+#Given the authors age in seconds (1,160 million seconds), how old is he in years?
+puts '1,160 million seconds old = ' + (1160000000 / 60 / 60 / 24 / 365).to_s + ' years old.'
