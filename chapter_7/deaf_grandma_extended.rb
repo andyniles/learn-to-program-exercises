3a797046eeb048d88a7f935c5781a38122aefbcc
Andy Niles <aniles@gmail.com>
2014-02-23 16:26:34 -0500

    modified deaf grandma program and added deaf grandma extended program

--- chapter_7/deaf_grandma_extended.rb
+++ chapter_7/deaf_grandma_extended.rb
@@ -1,13 +1,20 @@
 #Deaf grandma
 
+bye_count = 0
 while true
   input = gets.chomp
   if input.upcase == input
     puts 'NO, NOT SINCE ' + (1930 + rand(21)).to_s
     if input == 'BYE'
-      break
+      bye_count += 1
+    else
+      bye_count = 0
     end
   else
     puts 'HUH?!  SPEAK UP, SONNY!'
+    bye_count = 0
+  end
+  if bye_count == 3
+    break
   end
 end
