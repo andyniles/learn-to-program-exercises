95644f0e8b1c23ff61d136d42262a3c3ef9fec2f
Andy Niles <aniles@gmail.com>
2014-02-23 16:21:42 -0500

    modified deaf grandma program

--- chapter_7/deaf_grandma.rb
+++ chapter_7/deaf_grandma.rb
@@ -3,8 +3,10 @@
 while true
   input = gets.chomp
   if input.upcase == input
-   puts 'NO, NOT SINCE ' + (1930 + rand(20)).to_s
-   break
+    puts 'NO, NOT SINCE ' + (1930 + rand(20)).to_s
+    if input == 'BYE'
+      break
+    end
   else
     puts 'HUH?!  SPEAK UP, SONNY!'
   end
